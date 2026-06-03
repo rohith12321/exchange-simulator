7b5a0fac2d125d5e441d94a0560d807d773606b7
Rohith <rohithrajeev01@gmail.com>
2026-06-04 01:24:31 +0530

    Add order header

---
+++ include/order.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+enum class Side{
+    BUY, 
+    SELL
+};
+
+enum class OrderType{
+    MARKET, 
+    LIMIT
+};
+
+class Order{
+    public:
+        long long orderId;
+        int traderId;
+        Side side;
+        OrderType type;
+        int quantity;
+        double price;
+        long long timestamp;
+
+        Order(
+            long long orderId, 
+            int traderId, 
+            Side side, 
+            OrderType type, 
+            int quantity, 
+            double price, 
+            long long timestamp
+        );
+};
